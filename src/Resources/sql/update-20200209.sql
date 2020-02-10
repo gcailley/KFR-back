87cf59560cbec14f16ee5c09b74edd6485f31c5a
gcailley <gregory.cailley@gmail.com>
2020-02-10 13:00:49 +0100

    commit vide pour installation

--- src/Resources/sql/update-20200209.sql
+++ src/Resources/sql/update-20200209.sql
@@ -2,3 +2,4 @@ CREATE TABLE rtlq_adherents_events (event_id INT NOT NULL, adherent_id INT NOT N
      ALTER TABLE rtlq_adherents_events ADD CONSTRAINT FK_406FD7F771F7E88B FOREIGN KEY (event_id) REFERENCES rtlq_event (id);
      ALTER TABLE rtlq_adherents_events ADD CONSTRAINT FK_406FD7F725F06C53 FOREIGN KEY (adherent_id) REFERENCES rtlq_adherent (id);
      ALTER TABLE rtlq_event ADD nb_accompagnants INT NOT NULL, ADD nb_people INT NOT NULL, CHANGE commentaire commentaire VARCHAR(100) DEFAULT NULL;
+     
